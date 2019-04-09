09a3b8d2c2a5b80da99cbe2d8f7a44919cd9cf08
Joe Tower <joe@fourkitchens.com>
2019-04-09 10:45:18 -0500

    Adding url

--- config/site.js
+++ config/site.js
@@ -1,18 +1,18 @@
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
-  title: 'Gatsby Tutorial Starter', // Navigation and Site Title
-  titleAlt: 'Gatsby Tutorial Starter', // Title for JSONLD
-  description: 'A Gatsby V2 Starter Template Built with a Step By Step Guide',
-  url: 'https://gatsby-tutorial-starter.netlify.com', // Domain of your site. No trailing slash!
-  siteUrl: 'https://gatsby-tutorial-starter.netlify.com', // url + pathPrefix
+  title: 'Joe Tower', // Navigation and Site Title
+  titleAlt: 'Joe Tower', // Title for JSONLD
+  description: 'Designer, frontend engineer, photographer, traveller.',
+  url: 'https://inspiring-gates-5ae468.netlify.com', // Domain of your site. No trailing slash!
+  siteUrl: 'https://inspiring-gates-5ae468.netlify.com/', // url + pathPrefix
   siteLanguage: 'en', // Language Tag on <html> element
   logo: 'static/logo/logo.png', // Used for SEO
   banner: 'static/logo/banner.png',
   // JSONLD / Manifest
   favicon: 'static/logo/favicon.png', // Used for manifest favicon generation
   shortName: 'GatsbyTut', // shortname for manifest. MUST be shorter than 12 characters
-  author: 'Justin', // Author for schemaORGJSONLD
+  author: 'Joe Tower', // Author for schemaORGJSONLD
   themeColor: '#3e7bf2',
   backgroundColor: '#d3e0ff',
-  twitter: '@justinformentin', // Twitter Username
+  twitter: '@joeydollaz', // Twitter Username
 };
